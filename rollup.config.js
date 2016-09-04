fc311578560103574cd7d7e9703a057f3c3c0c55
Adrien Antoine <adriantoine@gmail.com>
2016-09-04 18:22:05 +0100

    Switched to rollup for the build to provide a jsnext:main

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import path from 'path'
+import babel from 'rollup-plugin-babel'
+import nodeResolve from 'rollup-plugin-node-resolve'
+
+const pkg = require('./package.json')
+
+const external = Object.keys(pkg.dependencies).concat(Object.keys(pkg.peerDependencies))
+
+export default {
+  entry: path.resolve(__dirname, 'index.js'),
+  plugins: [
+    babel(),
+    nodeResolve({
+      jsnext: true,
+      main: true,
+    }),
+  ],
+  external,
+  targets: [
+    {
+      dest: path.resolve(__dirname, pkg.main),
+      format: 'umd',
+      moduleName: 'depensitor',
+      sourceMap: true,
+    },
+    {
+      dest: path.resolve(__dirname, pkg['jsnext:main']),
+      format: 'es',
+      sourceMap: true,
+    },
+  ],
+}
